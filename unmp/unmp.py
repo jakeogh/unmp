4e9006c20074541b6180f9c549f5ed0ba4b63fa0
jakeogh <github.com@v6y.net>
2022-01-02 00:13:38 -0700

    auto-commit

--- unmp/unmp.py
+++ unmp/unmp.py
@@ -41,9 +41,10 @@ signal(SIGPIPE, SIG_DFL)
 
 def unmp(buffer_size):
     unpacker = msgpack.Unpacker()
-    current_buffer = sys.stdin.buffer.read(buffer_size)
+    #current_buffer = sys.stdin.buffer.read(buffer_size)
     #while len(current_buffer) > 0:
     for chunk in iter(lambda: sys.stdin.buffer.read(buffer_size), b""):
+        ic(chunk)
         #if len(current_buffer) == 0:
         #    break
         unpacker.feed(chunk)
