d149bdb4ff98e497bc2fe82f3c05a9e221cf837c
jakeogh <github.com@v6y.net>
2022-07-18 20:17:17 -0700

    autocommit

---
+++ unmp/__init__.py
@@ -0,0 +1,1 @@
+from .unmp import unmp
--- unmp/unmp.py
+++ unmp/unmp.py
@@ -26,16 +26,68 @@ from math import inf
 from signal import SIG_DFL
 from signal import SIGPIPE
 from signal import signal
-
+from typing import Iterator
+from typing import BinaryIO
+import msgpack
 import click
 from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
 from epprint import epprint
 from eprint import eprint
-from mptool import unmp as _unmp
+from typing import Type
 
 signal(SIGPIPE, SIG_DFL)
+
+# verbose can be math.inf
+def unmp(
+    *,
+    verbose: bool | int | float,
+    valid_types: None | list | tuple = None,
+    buffer_size: int = 1024,
+    skip: None | int = None,
+    single_type: bool = True,
+    strict_map_key: bool = False,  # True is the default
+    file_handle: BinaryIO = sys.stdin.buffer,
+) -> Iterator[object]:
+
+    unpacker = msgpack.Unpacker(strict_map_key=strict_map_key, use_list=False)
+    index = 0
+    if valid_types:
+        for _type in valid_types:
+            if not isinstance(_type, Type):
+                raise ValueError(
+                    f"valid_types was passed with a non-Type member {_type=}"
+                )
+
+    found_type: Type = type(None)
+    for chunk in iter(lambda: file_handle.read(buffer_size), b""):
+        if verbose == inf:
+            epprint(
+                f"{valid_types=}",
+                f"{buffer_size=}",
+                f"{type(chunk)=}," f"{len(chunk)=}",
+                f"{chunk=}",
+            )
+        unpacker.feed(chunk)
+        for value in unpacker:
+            if single_type:
+                if index == 0:
+                    found_type = type(value)
+                else:
+                    if not isinstance(value, found_type):
+                        raise TypeError(f"{value=} does not match {found_type=}")
+            index += 1
+            if verbose == inf:
+                epprint(f"{index=}", f"{value=}")
+            if skip is not None:
+                if index <= skip:
+                    continue
+            if valid_types is not None:
+                if type(value) not in valid_types:
+                    raise TypeError(f"{type(value)} not in valid_types: {valid_types}")
+            yield value
+
 
 
 @click.command()
@@ -85,7 +137,7 @@ def cli(
             sys.stdin.close()
             sys.exit(1)
 
-    unpacker = _unmp(
+    unpacker = unmp(
         buffer_size=buffer_size,
         strict_map_key=strict_map_key,
         verbose=verbose,
@@ -108,3 +160,4 @@ def cli(
             sys.stdout.flush()
         else:
             raise NotImplementedError(type(value))
+
