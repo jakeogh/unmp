c95eb6c7b72c009a2a33f6dd43e50306e188e29d
jakeogh <github.com@v6y.net>
2023-06-26 14:32:26 -0700

    auto-commit

--- unmp/unmp.py
+++ unmp/unmp.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import sys
 from collections.abc import Iterator
+from contextlib import suppress
 from typing import BinaryIO
 from typing import TypeVar
 
@@ -18,7 +19,7 @@ T = TypeVar("T", list, tuple)
 
 def unmp(
     *,
-    valid_types: None | list | tuple = None,
+    valid_types: list | tuple = (),
     buffer_size: int = 1024,
     skip: None | int = None,
     single_type: bool = True,
@@ -28,8 +29,10 @@ def unmp(
     verbose: bool | int | float = False,
 ) -> Iterator[object]:
     _unpacker_options = {"strict_map_key": strict_map_key, "use_list": False}
+    _suppress_exceptions = []
     if ignore_errors:
         _unpacker_options["unicode_errors"] = "ignore"
+        _suppress_exceptions = [ValueError]
     # unpacker = msgpack.Unpacker(strict_map_key=strict_map_key, use_list=False)
     unpacker = msgpack.Unpacker(**_unpacker_options)
     index = 0
@@ -50,19 +53,22 @@ def unmp(
                 f"{chunk=}",
             )
         unpacker.feed(chunk)
-        for value in unpacker:
-            if single_type:
-                if index == 0:
-                    found_type = type(value)
-                elif not isinstance(value, found_type):
-                    raise TypeError(f"{value=} does not match {found_type=}")
-            index += 1
-            if gvd:
-                epprint(f"{index=}", f"{value=}")
-            if skip is not None:
-                if index <= skip:
-                    continue
-            if valid_types is not None:
-                if type(value) not in valid_types:
-                    raise TypeError(f"{type(value)} not in valid_types: {valid_types}")
-            yield value
+        with suppress(*_suppress_exceptions):
+            for value in unpacker:
+                if single_type:
+                    if index == 0:
+                        found_type = type(value)
+                    elif not isinstance(value, found_type):
+                        raise TypeError(f"{value=} does not match {found_type=}")
+                index += 1
+                if gvd:
+                    epprint(f"{index=}", f"{value=}")
+                if skip is not None:
+                    if index <= skip:
+                        continue
+                if valid_types is not None:
+                    if type(value) not in valid_types:
+                        raise TypeError(
+                            f"{type(value)} not in valid_types: {valid_types}"
+                        )
+                yield value
