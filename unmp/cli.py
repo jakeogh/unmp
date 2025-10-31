bfafe01d484a5a4268863a82ccafbd3de152fd19
jakeogh <github.com@v6y.net>
2025-10-31 00:40:14 -0700

    auto-commit

--- unmp/cli.py
+++ unmp/cli.py
@@ -1,90 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
-
 from __future__ import annotations
-
 import sys
+import argparse
 from signal import SIG_DFL
 from signal import SIGPIPE
 from signal import signal
-
-import click
-from asserttool import ic
-from clicktool import click_add_options
-from clicktool import click_global_options
-from clicktool import tvicgvd
-from epprint import epprint
 from eprint import eprint
-from globalverbose import gvd
-
 from unmp import unmp
 
 signal(SIGPIPE, SIG_DFL)
 
-
-@click.command()
-@click.option(
-    "-r",
-    "--repr",
-    "use_repr",
-    is_flag=True,
-)
-@click.option(
-    "--strict-map-key",
-    is_flag=True,
-)
-@click.option(
-    "--buffer",
-    "buffer_size",
-    type=int,
-    default=16384,
-)
-@click_add_options(click_global_options)
-@click.pass_context
 def cli(
-    ctx,
     buffer_size: int,
-    verbose_inf: bool,
     use_repr: bool,
-    dict_output: bool,
     strict_map_key: bool,
-    verbose: bool = False,
 ) -> None:
-    assert not dict_output
-
-    tty, verbose = tvicgvd(
-        ctx=ctx,
-        verbose=verbose,
-        verbose_inf=verbose_inf,
-        ic=ic,
-        gvd=gvd,
-    )
-
-    if verbose_inf:
-        gvd.enable()
-
-    if tty:
+    if sys.stdout.isatty():
         if not use_repr:
             eprint(
                 "stdout is a tty, refusing to attempt to write arb py objects to it, use --repr"
             )
             sys.stdin.close()
             sys.exit(1)
-
     unpacker = unmp(
         buffer_size=buffer_size,
         strict_map_key=strict_map_key,
     )
-
     for value in unpacker:
-        if verbose:
-            epprint(f"{type(value)=}", f"{value=}")
         if use_repr:
-            # in this case, the values are serialized, so it's correct for human/tty use to add '\n'
             sys.stdout.write(repr(value) + "\n")
             sys.stdout.flush()
             continue
-
         if isinstance(value, bytes):
             sys.stdout.buffer.write(value)
             sys.stdout.buffer.flush()
@@ -93,3 +40,29 @@ def cli(
             sys.stdout.flush()
         else:
             raise NotImplementedError(type(value))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "--repr",
+        dest="use_repr",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--strict-map-key",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--buffer",
+        dest="buffer_size",
+        type=int,
+        default=16384,
+    )
+    args = parser.parse_args()
+
+    cli(
+        buffer_size=args.buffer_size,
+        use_repr=args.use_repr,
+        strict_map_key=args.strict_map_key,
+    )
